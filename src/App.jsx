17d9d6f546ab472cfe8adb9c7e351ca62a6f75a1
Dawe116 <molnar.dawe116@gmail.com>
2024-11-13 09:16:28 +0100

    FrontEnd Chess

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter as Router, NavLink, Routes, Route} from 'react-router-dom';
+import './App.css';
+import {ChessList} from './ChessList'
+
+function App() {
+  return (
+    <Router>
+      <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
+        <div className='collapse navbar-collapse' id='navbarNav'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <NavLink className='nav-link' to='/'>
+                Sakkozók
+              </NavLink>
+            </li>
+          </ul>
+          </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<ChessList />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
